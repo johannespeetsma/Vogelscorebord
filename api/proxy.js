7856e534a0a1172f47f70c22c31f160e7cd27249
johannespeetsma <johannespeetsma@gmail.com>
2026-03-13 09:26:10 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -4,7 +4,7 @@ module.exports = async function handler(req, res) {
   if (!user_id) return res.status(400).json({ error: 'Geen user_id' });
 
   const sessionCookie = 'sessionid=r39oo7t53bcizqmolb5g5mw3mx00vtpi';
-  const url = `https://waarneming.nl/users/${user_id}/species/?period=year&species_group_id=1&filter_year=${year}`;
+  const url = `https://waarneming.nl/users/${user_id}/species/?period=life&species_group_id=1&filter_year=${year}`;
 
   const response = await fetch(url, {
     headers: {
@@ -16,22 +16,13 @@ module.exports = async function handler(req, res) {
 
   const html = await response.text();
 
-  // Tel tabelrijen maar trek navigatie-rijen eraf
   const tableRows = (html.match(/<tr>/g) || []).length;
-  
-  // Zoek de tabel met soorten - rijen met species links
   const speciesRows = (html.match(/\/species\/\d+/g) || []);
   const uniqueSpecies = new Set(speciesRows).size;
 
-  // Zoek ook naar het getal direct in de paginatitel of header
-  const titleMatch = html.match(/(\d+)\s*soort(?:en)?\s*gezien/i) ||
-                     html.match(/Totaal[^\d]*(\d+)/i) ||
-                     html.match(/<h[1-4][^>]*>.*?(\d+).*?<\/h[1-4]>/i);
-
   return res.status(200).json({ 
-    species_count: uniqueSpecies || tableRows,
+    species_count: tableRows,
     unique_species: uniqueSpecies,
     table_rows: tableRows,
-    title_match: titleMatch ? titleMatch[1] : null
   });
 }
