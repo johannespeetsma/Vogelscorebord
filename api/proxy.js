1a75a56560af4c8e8f7a0c932935e1d1260348b5
johannespeetsma <johannespeetsma@gmail.com>
2026-03-13 09:15:35 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
     // Debug: stuur een stukje HTML terug
     return res.status(200).json({ 
       error: 'Kon aantal niet vinden', 
-    html_preview: html.substring(9000, 13000)
+    html_preview: html.substring(13000, 17000)
     });
 
   } catch (e) {
